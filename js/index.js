7f080deddd3da68daa558abbf39409fc308c68f8
Auriane VIGGIANO <auriane.vigg@gmail.com>
2021-09-02 21:14:17 +0200

    iteration 1 fini

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 // Iteration 1: Names and Input
+const hacker1 = "Auriane"
+// 1.2
+console.log(`The driver's name is ${hacker1}`)
 
+// 1.3
+const hacker2 = "Léa"
+
+//1.4
+console.log(`The navigator's name is ${hacker2}`)
 
 // Iteration 2: Conditionals
 
