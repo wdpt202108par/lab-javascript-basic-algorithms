5a4206bb8d87bad97f9a29de4ad4fc3c2f5e8756
kthomas <kevin.allan.thomas@gmail.com>
2021-09-02 21:28:46 +0200

    iteration 1 finished et iteration 2 started

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 // Iteration 1: Names and Input
+let hacker1 = 'kevin';
+console.log ("The driver's name is " +  hacker1)
 
+let hacker2 = 'cindy';
+console.log ("The driver's name is " +  hacker2)
 
 // Iteration 2: Conditionals
-
+if (hacker1.length === hacker2.length) {
+    console.log(`The driver\'s has the longest name, it has ${hacker1.length} characters`)
+}
 
 // Iteration 3: Loops
