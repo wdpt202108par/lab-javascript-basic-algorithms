a9867ed613a194cc7af5fc49fb19cf56ba3672f0
evanina <eva.degano@gmail.com>
2021-09-02 20:43:47 +0200

    did iteration 2

--- js/index.js
+++ js/index.js
@@ -6,6 +6,6 @@ let hacker2 = 'Eva';
 console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
+console.log(`The driver has the longest name, it has ${hacker1.length} characters.`);
 
 // Iteration 3: Loops
